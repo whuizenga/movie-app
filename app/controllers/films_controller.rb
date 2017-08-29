0e0e303db59ea8b81080f9b5d67b6199e634db16
whuizenga <will.huizenga@gmail.com>
2017-08-29 15:43:58 -0400

    got show route to work

--- app/controllers/films_controller.rb
+++ app/controllers/films_controller.rb
@@ -1,4 +1,32 @@
 class FilmsController < ApplicationController
+
     def index
+        @films = Film.all
+    end
+
+    def new
+        @film = Film.new
+    end
+
+    def create
+        @film = Film.create!(film_params)
+
+        redirect_to "/"
+    end
+
+    def edit
+        @film = Film.find params[:id]
+    end
+
+    def update
+    end
+
+    def show
+        @film = Film.find params[:id]
+    end
+
+    private
+    def film_params
+        params.require(:film).permit(:title, :genre, :year, :image, :synopsis)
     end
 end
